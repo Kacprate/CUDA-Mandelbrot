e94a5ea76e872771fa9dbc2c58c4a41a1802a2c7
Kacprate <kacpratecontact@gmail.com>
2020-05-25 23:39:25 +0200

    bugfix

--- main.py
+++ main.py
@@ -271,7 +271,7 @@ while running:
             center['y'] += movey / scale
         renderHandler(movex, movey, update)
     display.blit(surf, (0, 0))
-    fps = 1000
+    fps = 60
     if deltaTime != 0:
         fps = 1/deltaTime
     if AUTO_LOWER_CURSORSPEED and fps < 15 and cursorSpeed > 300:
